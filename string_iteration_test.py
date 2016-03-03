1e6788ceea3dc90ec3e69fc98500886dacd98059
tollo <andrew.miatello@gmail.com>
2016-03-03 22:09:15 +1000

    Updates

--- string_iteration_test.py
+++ string_iteration_test.py
@@ -1,13 +1,15 @@
 # Iteration through a string
-"""
-i = 0
 
-for i in "abcdefghij":
-    i = int(input(i))
-"""
+
+
+
 d={}
 for x in range(1,11):
     d['Integer_{0}'.format(x)]=int(input('Integer_' + str(x) + ':'))
+
+for x in range(1,11):
+    print(d['Integer_{0}'.format(x)])
+
 
 print(d['Integer_5'])
 
