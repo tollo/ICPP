1718f2c39b699cd7e343d6598c05f5dd7e0c3e72
tollo <andrew.miatello@gmail.com>
2016-03-07 21:00:25 +1000

    final file

--- root_and_power.py
+++ root_and_power.py
@@ -1,19 +1,27 @@
 # Express a number as a root and power
 
-x = int(input('Enter an integer: '))
+while True:
+    try:
+        x = int(input('Enter an Integer: '))
+        break
+    except ValueError:
+        print('Please enter only integers')
 
-pwr = 6
+for root in range(2,abs(x)):
+    pwr = 0
+    while root**pwr < abs(x):
+        pwr = pwr + 1
+    if root**pwr == abs(x):
+        break
 
-ans = 0
-
-while ans**pwr < abs(x):
-    ans = ans + 1
-
-if ans**pwr != abs(x):
-    #decrement pwr here - but how to stop it? for loop?
-    print(x, 'is not a perfect power of 1-6')
+if root**pwr != abs(x):
+    print(x, 'is not a perfect power')
 else:
-    if x < 0:
-        ans = -ans
-    print(ans, 'to the power of', pwr, 'is', x)
+    if x < 0 and pwr%2 != 1:
+        print('Imaginary Number')
+    elif x < 0:
+        root = -root
+        print(root, 'to the power of', pwr, 'is', x)
+    else:
+        print(root, 'to the power of', pwr, 'is', x)
 
