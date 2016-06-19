bb3c77ba4db12d03499a3e3f753a1b8d82973b8d
Andrew Miatello <andrew.miatello@gmail.com>
2016-06-19 20:53:04 +1100

    updated for global variables

--- fibonacci.py
+++ fibonacci.py
@@ -12,6 +12,8 @@ while True:
 def fib(n):
     """Assumes that n is an int > 0
        Returns Fibonacci of n"""
+    global numFibCalls
+    numFibCalls += 1
     if n == 0 or n == 1:
         return 1
     else:
@@ -20,7 +22,10 @@ def fib(n):
 
 def testFib(n):
     for i in range(n+1):
+        global numFibCalls
+        numFibCalls = 0
         print('fib of ', i, '=', fib(i))
+        print('fib called', numFibCalls, 'times')
 
 
 testFib(input_value)
