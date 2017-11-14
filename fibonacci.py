31c28a989673dab507371630aa3340d788800aab
tollo <andrew.miatello@gmail.com>
2017-11-14 20:37:51 +1100

    File updated to be PEP8 compliant

--- fibonacci.py
+++ fibonacci.py
@@ -1,31 +1,40 @@
-# Print fibonacci number
+# fibonacci.py
+""" Prints the fibonacci number of an input integer
+    Uses global variables
+    Uses a test program to test the number of program calls"""
+
+# Defining global variable to use across functions
+NUM_FIB_CALLS = 0
 
 
-while True:
-    try:
-        input_value = int(input('Integer: '))
-        break
-    except ValueError:
-        print('Please enter only integers')
-
-
-def fib(n):
-    """Assumes that n is an int > 0
-       Returns Fibonacci of n"""
-    global numFibCalls
-    numFibCalls += 1
-    if n == 0 or n == 1:
+def fib(input_integer):
+    """ Assumes that input_integer is an int > 0
+        Returns Fibonacci of input_integer"""
+    global NUM_FIB_CALLS
+    NUM_FIB_CALLS += 1
+    if input_integer == 0 or input_integer == 1:
         return 1
-    else:
-        return fib(n-1) + fib(n-2)
+    return fib(input_integer-1) + fib(input_integer-2)
 
 
-def testFib(n):
-    for i in range(n+1):
-        global numFibCalls
-        numFibCalls = 0
+def test_fib(input_integer):
+    """Test program for fib"""
+    for i in range(input_integer+1):
+        global NUM_FIB_CALLS
+        NUM_FIB_CALLS = 0
         print('fib of ', i, '=', fib(i))
-        print('fib called', numFibCalls, 'times')
+        print('fib called', NUM_FIB_CALLS, 'times')
 
 
-testFib(input_value)
+def main():
+    """Main program"""
+    while True:
+        try:
+            input_value = int(input('Integer: '))
+            break
+        except ValueError:
+            print('Please enter only integers')
+    test_fib(input_value)
+
+
+main()
