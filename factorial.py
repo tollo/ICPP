ad2679946a76cb5c359484cce65fd98e5f65bcba
tollo <andrew.miatello@gmail.com>
2017-11-14 19:54:30 +1100

    File updated to be PEP8 compliant

--- factorial.py
+++ factorial.py
@@ -1,32 +1,37 @@
-# Print factorials
+# factorial.py
+"""Prints the factorial of an integer twice:
+    once using iteration,
+    and a second time using recursion"""
 
 
-while True:
-    try:
-        input_value = int(input('Integer: '))
-        break
-    except ValueError:
-        print('Please enter only integers')
-
-
-def factI(n):
-    """Assumes that n is an int > 0
+def fact_i(input_integer):
+    """Assumes that input_integer is an int > 0
        Returns n!"""
     result = 1
-    while n > 1:
-        result = result * n
-        n -= 1
+    while input_integer > 1:
+        result = result * input_integer
+        input_integer -= 1
     return result
 
 
-def factR(n):
-    """Assumes that n is an int > 0
+def fact_r(input_integer):
+    """Assumes that input_integer is an int > 0
        Returns n!"""
-    if n == 1:
-        return n
-    else:
-        return n*factR(n - 1)
+    if input_integer == 1:
+        return input_integer
+    return input_integer*fact_r(input_integer - 1)
 
-print(input_value, 'factorial is: ', factI(input_value))
 
-print(input_value, 'factorial is: ', factR(input_value))
+def main():
+    """Main program"""
+    while True:
+        try:
+            input_value = int(input('Integer: '))
+            break
+        except ValueError:
+            print('Please enter only integers')
+    print(input_value, 'factorial is: ', fact_i(input_value))
+    print(input_value, 'factorial is: ', fact_r(input_value))
+
+
+main()
