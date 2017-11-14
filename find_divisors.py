e19d2bf47826744863a20ac0451ad916e9db43a1
tollo <andrew.miatello@gmail.com>
2017-11-14 20:59:38 +1100

    File updated to be PEP8 compliant

--- find_divisors.py
+++ find_divisors.py
@@ -1,34 +1,39 @@
-# Find common divisors of two numbers
-# Introduction to Compututation and Programming uising Python
-
-d = {}
-for x in 'ab':
-    # Error handling for data types other than 'int'
-    while True:
-        try:
-            input_value = int(input('Integer_' + str(x) + ': '))
-            break
-        except ValueError:
-            print('Please enter only integers')
-    d['Integer_{0}'.format(x)] = input_value
+# find_divisors.py
+""" Find common divisors of two numbers
+    Exercises from Introduction to Compututation and Programming uising Python"""
 
 
-def findDivisors(n1, n2):
-    """Assumes that n1 and n2 are positive integers
-       Returns a tuple containing all common divisors of n1 and n2"""
-    divisors = ()  # the empty tuple
-    for i in range(1, min(n1, n2) + 1):
-        if n1 % i == 0 and n2 % 1 == 0:
-            divisors = divisors + (i,)
-    return divisors
+def main():
+    """ Main function"""
+    dict_list = {}
+    for num in 'ab':
+        # Error handling for data types other than 'int'
+        while True:
+            try:
+                input_value = int(input('Integer_' + str(num) + ': '))
+                break
+            except ValueError:
+                print('Please enter only integers')
+        dict_list['Integer_{0}'.format(num)] = input_value
 
-divisors = findDivisors(d['Integer_a'], d['Integer_b'])
-print(divisors)
-total = 0
-for d in divisors:
-    total += d
-print(total)
 
+    def find_divisors(n_1, n_2):
+        """Assumes that n1 and n2 are positive integers
+        Returns a tuple containing all common divisors of n1 and n2"""
+        divisors = ()  # the empty tuple
+        for i in range(1, min(n_1, n_2) + 1):
+            if n_1 % i == 0 and n_2 % 1 == 0:
+                divisors = divisors + (i,)
+        return divisors
+
+    divisors = find_divisors(dict_list['Integer_a'], dict_list['Integer_b'])
+    print(divisors)
+    total = 0
+    for _d_ in divisors:
+        total += _d_
+    print(total)
+
+main()
 
 # def findDivisors(n1, n2):
 #    """Assumes that n1 and n2 are positive integers
