e3bb2b89ed1ff144c2081d922d28c3a39ed144a9
tollo <andrew.miatello@gmail.com>
2017-11-14 20:44:57 +1100

    File updated to PEP8 compliant

--- cube_root_bisection.py
+++ cube_root_bisection.py
@@ -1,29 +1,34 @@
-# Cube root using bisection search
-# Introduction to Computation and Programming Using Python
+# cube_root_bisection.py
+""" Find cube root using bisection search
+    Excercise from Introduction to Computation and Programming Using Python"""
 
+def main():
+    """ Main program"""
+    while True:
+        try:
+            input_value = int(input('Integer: '))
+            break
+        except ValueError:
+            print('Please enter only integers')
 
-while True:
-    try:
-        input_value = int(input('Integer: '))
-        break
-    except ValueError:
-        print('Please enter only integers')
-
-x = abs(input_value)
-epsilon = 0.01
-numGuesses = 0
-low = 0.0
-high = max(1.0, x)
-ans = (high + low) / 2.0
-while abs(ans**3 - x) >= epsilon:
-    print('low =', low, 'high =', high, 'ans =', ans)
-    numGuesses += 1
-    if ans**3 < x:
-        low = ans
-    else:
-        high = ans
+    abs_val_input = abs(input_value)
+    epsilon = 0.01
+    num_guesses = 0
+    low = 0.0
+    high = max(1.0, abs_val_input)
     ans = (high + low) / 2.0
-print('numGuesses =', numGuesses)
-if input_value < 0:
-    ans = - ans
-print(ans, 'is close to square root of', x)
+    while abs(ans**3 - abs_val_input) >= epsilon:
+        print('low =', low, 'high =', high, 'ans =', ans)
+        num_guesses += 1
+        if ans**3 < abs_val_input:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low) / 2.0
+    print('num_guesses =', num_guesses)
+    if input_value < 0:
+        ans = - ans
+    print(ans, 'is close to square root of', abs_val_input)
+
+
+main()
