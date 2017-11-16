d3b59d203a9ee6a4be8fc6e05eb7f84fd383484e
tollo <andrew.miatello@gmail.com>
2017-11-16 20:25:58 +1100

    File updated to be PEP8 compliant

--- largest_of_ten_v1.py
+++ largest_of_ten_v1.py
@@ -1,35 +1,37 @@
-# Compares ten variables and returns the largest odd number of the ten
+# largest_of_ten_v1.py
+""" Compares ten variables and returns the largest odd number of the ten"""
 
 
-def program():
+def main():
+    """ Main program"""
 
     print('Input ten integers')
 
     # Create a dictionary to store values
-    d = {}
-    for x in range(1, 11):
+    dict_list = {}
+    for num in range(1, 11):
         # Error handling for data types other than 'int'
         while True:
             try:
-                input_value = int(input('Integer_' + str(x) + ': '))
+                input_value = int(input('Integer_' + str(num) + ': '))
                 break
             except ValueError:
                 print('Please enter only integers')
         # Remove even numbers
-        if (input_value % 2 == 0):
+        if input_value % 2 == 0:
             input_value = False
         # Populate dictionary
-        d['Integer_{0}'.format(x)] = input_value
+        dict_list['Integer_{0}'.format(num)] = input_value
 
     # Iterate and compare dictionary values
     largest_number = False
-    for x in range(1, 11):
-        if d['Integer_{0}'.format(x)] > largest_number:
-            largest_number = d['Integer_{0}'.format(x)]
+    for num in range(1, 11):
+        if dict_list['Integer_{0}'.format(num)] > largest_number:
+            largest_number = dict_list['Integer_{0}'.format(num)]
 
     if largest_number is False:
         print('No number is odd')
     else:
         print('The largest odd number entered is: ' + str(largest_number))
 
-program()
+main()
