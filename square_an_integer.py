6ae7a86f34a288d0907f6d64ca9ee2fb8a3d0454
tollo <andrew.miatello@gmail.com>
2017-11-17 19:23:45 +1100

    File updated to be PEP8 compliant

--- square_an_integer.py
+++ square_an_integer.py
@@ -1,15 +1,21 @@
-# Square an integer the long way
+# square_an_integer.py
+""" Square an integer the long way"""
 
-x = int(input('Enter a number: '))
+def main():
+    """ Main program"""
 
-ans = 0
+    while True:
+        try:
+            input_value = int(input('Please enter an integer: '))
+            break
+        except ValueError:
+            print('Please enter only integers')
+    ans = 0
+    iters_left = abs(input_value)
+    while iters_left != 0:
+        ans = ans + abs(input_value)
+        iters_left = iters_left - 1
+    print(str(input_value) + '*' + str(input_value) + ' = ' + str(ans))
 
-itersLeft = abs(x)
 
-while (itersLeft != 0):
-    ans = ans + abs(x)
-    itersLeft = itersLeft - 1
-
-print(str(x) + '*' + str(x) + ' = ' + str(ans))
-
-
+main()
